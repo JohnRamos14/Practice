a5c18679a97fcfdc4010ea18d266136a7357621e
John Ramos <ramos.johnmba@gmail.com>
2022-12-09 08:37:52 -0800

    unsplash edit

--- axiosPractice.js
+++ axiosPractice.js
@@ -1,43 +1,46 @@
 const getJsonData = () => {
-    practiceService
-     .get()
-     .then(onGetSuccess)
-     .catch(onGetError);}
+  practiceService.get().then(onGetSuccess).catch(onGetError);
+};
 
-     function onGetSuccess(response){
+function onGetSuccess(response) {
+  let result = response.data;
+  result.map((item) => {
+    let div = document.createElement("div");
+    div.classList.add("card");
+    
+    let img = document.createElement("img");
+    img.src = item.thumbnailUrl;
+    div.appendChild(img);
+    let title = document.createElement("h3");
+    title.innerHTML = item.title;
+    div.appendChild(title);
 
-        let result = response.data;
-        result.map((item) => {
-            let div = document.createElement("div");
-            div.innerHTML = JSON.stringify(item);
-            document.body.appendChild(div);
+    document.body.appendChild(div);
+  });
 
-        })
+  console.log("onGetSuccess is firing", result);
+}
 
+function onGetError(response) {
+  console.error({ error: response });
+}
 
-         console.log("onGetSuccess is firing")
-     }
+getJsonData();
 
-     function onGetError(response){
-        console.error({error : response})
-     }
+//  const getById = () => {
+//         practiceService
+//         .getById(1)
+//         .then(onGetByIdSuccess)
+//         .catch(onGetByIdError)
+//     }
 
-     getJsonData();
+//     function onGetByIdSuccess(response){
+//         console.log("onGetByIdSuccess is firing")
+//         console.log(response.data, "GET BY ID")
+//     }
 
-    //  const getById = () => {
-    //         practiceService
-    //         .getById(1)
-    //         .then(onGetByIdSuccess)
-    //         .catch(onGetByIdError)
-    //     }
+//     function onGetByIdError(response){
+//         console.error({error : response})
+//     }
 
-    //     function onGetByIdSuccess(response){
-    //         console.log("onGetByIdSuccess is firing")
-    //         console.log(response.data, "GET BY ID")
-    //     }
-
-    //     function onGetByIdError(response){
-    //         console.error({error : response})
-    //     }
-
-    //     getById();
+//     getById();
---
+++ unsplashService.js
@@ -0,0 +1,3 @@
+const api = createApi({
+    accessKey: "FPPOlksZZAIFyWayBGT6XTkT1JFdZWPY5jP_ePzKvTM",
+});
