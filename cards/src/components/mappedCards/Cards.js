78fa93b7fd28beecdb3ac131f955227f2e7aa600
JohnRamos14 <Ramos.Johnmba@gmail.com>
2022-12-13 07:18:45 -0800

    packages

--- cards/src/components/mappedCards/Cards.js
+++ cards/src/components/mappedCards/Cards.js
@@ -1,22 +1,17 @@
 import React from "react";
-import { Card, Container, Row, Col } from "react-bootstrap";
+import { Card,Col } from "react-bootstrap";
 
 const Cards = (props) => {
-  let image = props.card.urls.regular
+  let image = props.card.urls.regular;
   return (
     <>
-    <Container>
-      <Row className="justify-center">
-      <Col lg={4}>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={image} />
+      <Card>
+        <Col>
+          <Card.Img variant="top" src={image} className="img" />
+        </Col>
       </Card>
-      </Col>
-      </Row>
-      </Container>
     </>
   );
 };
 
 export default React.memo(Cards);
-
--- cards/src/components/nav/MainNav.js
+++ cards/src/components/nav/MainNav.js
@@ -2,25 +2,22 @@ import { Button, Container, Form, Navbar } from "react-bootstrap";
 import React, { useState } from "react";
 import { search } from "../../services/unsplashService";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Cards from "../mappedCards/Cards"
+import Cards from "../mappedCards/Cards";
+import { Row } from "react-bootstrap";
 
 const MainNav = () => {
   const [formData, setFormData] = useState("");
-  const [cards, setCards] = useState([]);
-
+  const [cardsArr, setCards] = useState([]);
 
   const onSearchFromChange = (e) => {
     setFormData(e.target.value);
-  
   };
 
   const onSearchClick = (e) => {
     console.log("useEffect is firing");
     let query = formData;
     console.log("query");
-    search(query)
-      .then(onSearchSuccess)
-      .catch(onSearchError);
+    search(query).then(onSearchSuccess).catch(onSearchError);
   };
   const onSearchSuccess = (response) => {
     console.log("onSearchSuccess is firing");
@@ -32,8 +29,8 @@ const MainNav = () => {
   };
 
   const mapCards = (card) => {
-    return <Cards card={card} key={"Card-" + card.id}/>
-  }
+    return <Cards card={card} key={"Card-" + card.id} />;
+  };
 
   const onSearchError = (response) => {
     console.error({ error: response });
@@ -41,7 +38,7 @@ const MainNav = () => {
 
   return (
     <>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" expand="lg" className="nav-all">
         <Container fluid className="justify-content-center">
           <Form className="d-flex">
             <Form.Control
@@ -64,7 +61,10 @@ const MainNav = () => {
           </Form>
         </Container>
       </Navbar>
-      <div className="row">{cards.map(mapCards)}</div>  
+
+      <Container>
+        <Row>{cardsArr.map(mapCards)}</Row>
+      </Container>
     </>
   );
 };
